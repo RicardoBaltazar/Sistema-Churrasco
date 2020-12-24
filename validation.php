f20446b63c8ace7f78af9835e7a8f09eb014d6b1
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-12-23 23:52:32 -0300

    feature - criando validação do formulário

--- validation.php
+++ validation.php
@@ -8,6 +8,36 @@ if(isset($_POST['action'])){
         $nameGuest = $_POST['name-guest'];
         $beverageGuest = $_POST['beverage-guest'];
 
+        $Totalvalue = 10;
+        $beverageValue = 0;
+        $foodValue = 10;
+
+
+        if($beverage == '1'){
+            $beverage = 1;
+            $Totalvalue += 10;
+            $beverageValue += 10;
+        } else {
+            $beverage = 0;
+        }
+
+        if($guest == '1'){
+            $guest = 1;
+            $Totalvalue += 10;
+            $foodValue += 10;
+        } else {
+            $guest = 0;
+        }
+
+        if($beverageGuest == '1'){
+            $beverageGuest = 1;
+            $Totalvalue += 10;
+            $beverageValue += 10;   
+        } else {
+            $beverageGuest = 0;
+        }
+
+
         echo $name;
         echo "<br>";
         echo $beverage;
@@ -18,6 +48,14 @@ if(isset($_POST['action'])){
         echo "<br>";
         echo $beverageGuest;
         echo "<br>";
+        echo "<br>";
+        echo 'Valor total '.$Totalvalue;
+        echo "<br>";
+        echo "<br>";
+        echo 'Valor total da bebida '.$beverageValue;
+        echo "<br>";
+        echo "<br>";
+        echo 'Valor total da comida '.$foodValue;
 
         
         
